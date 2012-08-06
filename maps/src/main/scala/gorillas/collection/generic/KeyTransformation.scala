290894cf6791afe046793a67333f49592acceefe
Ricardo Leon <rmleon@home>
2012-08-06 00:45:20 -0400

    Implicit transformations

--- maps/src/main/scala/gorillas/collection/generic/KeyTransformation.scala
+++ maps/src/main/scala/gorillas/collection/generic/KeyTransformation.scala
@@ -10,3 +10,34 @@ trait KeyTransformation[K] {
   def apply(x: K): Int = transform(x)
 
 }
+
+/**
+ * Implicits for common types
+ */
+object KeyTransformation {
+  implicit object IntKeyTransformation extends KeyTransformation[Int] {
+    def transform(x: Int) = x
+  }
+
+  implicit object FloatKeyTransformation extends KeyTransformation[Float] {
+    final val divisor = Float.MaxValue / Int.MaxValue.toDouble
+    def transform(x: Float) = (x / divisor).toInt
+  }
+
+  implicit object DoubleKeyTransformation extends KeyTransformation[Double] {
+    final val divisor = Double.MaxValue / Int.MaxValue.toDouble
+    def transform(x: Double) = (x / divisor).toInt
+  }
+
+  implicit object LongKeyTransformation extends KeyTransformation[Long] {
+    final val divisor = Long.MaxValue / Int.MaxValue.toLong
+    def transform(x: Long) = (x / divisor).toInt
+  }
+
+// TODO: Implement transformation for string
+//  implicit object StringKeyTransformation extends KeyTransformation[String] {
+//    def transform(x: String) = {
+//    }
+//  }
+
+}
